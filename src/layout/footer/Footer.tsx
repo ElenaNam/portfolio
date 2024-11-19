4b6127cbb04279b75138724a7292e4e1a2ab2dac
ElenaNam <zaidelsonelena@gmail.com>
2024-11-19 18:02:31 +0400

    add styles to footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -4,12 +4,13 @@ import { FlexWrapper } from "../../components/FlexWrapper";
 import { MediaBox } from "./mediaBox/MediaBox";
 import { Logo } from "../../components/logo/Logo";
 import { Container } from "../../components/Container";
+import { theme } from "../../styles/Theme";
 
 export const Footer = () => {
   return (
     <StyledFooter>
       <Container>
-        <FlexWrapper justify="space-between">
+        <FlexWrapper justify="space-between" align="flex-start">
           <div>
             <FlexWrapper gap="24px">
               <Logo />
@@ -26,14 +27,25 @@ export const Footer = () => {
 };
 
 const StyledFooter = styled.footer`
+  margin-top: 89px;
   padding: 32px 0;
   border-top: 1px solid #abb2bf;
 `;
 const Link = styled.a``;
+
 const Text = styled.span`
+  display: inline-block;
+  margin-top: 16px;
   font-size: 16px;
+  line-height: 1.32;
+  color: ${theme.colors.main};
 `;
-const TextBottom = styled.div`
+
+const TextBottom = styled.span`
+  display: inline-block;
+  width: 100%;
   margin-top: 48px;
   text-align: center;
+  font-size: 16px;
+  line-height: 1.32;
 `;
--- src/layout/footer/mediaBox/MediaBox.tsx
+++ src/layout/footer/mediaBox/MediaBox.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Socials } from "../../../components/socials/Socials";
+import { theme } from "../../../styles/Theme";
 
 const dataSocials2 = [
   {
@@ -36,14 +37,20 @@ export const MediaBox = () => {
 };
 
 const StyledMediaBox = styled.div`
+  max-width: 112px;
+  width: 100%;
+
   ul {
     display: flex;
     justify-content: space-between;
+    font-size: 0;
   }
 `;
 
 const Title = styled.div`
   margin-bottom: 12px;
   font-size: 24px;
+  line-height: 1.3;
   font-weight: 500;
+  color: ${theme.colors.main};
 `;
